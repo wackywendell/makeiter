553f95b4d429b6ea99788222f64b0564fceea3c1
Wendell Smith <wendellwsmith@gmail.com>
2014-12-02 10:20:12 -0500

    Minimal example of rustc failure

--- src/bin/example.rs
+++
@@ -1,8 +0,0 @@
-extern crate makeiter;
-
-use makeiter::{make_iter,FuncIter};
-
-pub fn main() {
-    println!("{}", FuncIter::new(|| Some(3u)).next());
-    println!("{}", make_iter(|| Some(3u)).next());
-}
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ pub struct FuncIter<F> {
     f: F,
 }
 
-// `F` is the type of the unboxed closure
-// Is an unboxed closure because `F` implements the `FnOnce` trait
 impl<F, R> FuncIter<F> where F: FnMut() -> R {
     pub fn new(f: F) -> FuncIter<F> {
         FuncIter {
@@ -15,52 +13,8 @@ impl<F, R> FuncIter<F> where F: FnMut() -> R {
     }
 }
 
-impl<F, A> Iterator<A> for FuncIter<F> where F: FnMut() -> Option<A> {
-    fn next(&mut self) -> Option<A> {
-        self.f.call_mut(())
-    }
-}
-
-pub fn make_iter<F, A>(f : F) -> FuncIter<F> where F: FnMut() -> Option<A> {
-    FuncIter::new(f)
-}
-
 #[test]
 fn base_test() {
-    assert_eq!(FuncIter::new(|| Some(3u)).next(), Some(3u));
-    
     let val : Option<()> = None;
-    assert_eq!(FuncIter::new(|| val).next(), None);
-    
-    let i = 4u;
-    assert_eq!(FuncIter::new(|| Some(i)).next(), Some(i));
-    
-    let mut i = 4u;
-    let mut it = FuncIter::new(|| {
-        if i <= 0 {None}
-        else {i -= 1; Some(i)}
-    });
-    
-    assert_eq!(it.next(), Some(3));
-    assert_eq!(it.next(), Some(2));
-    assert_eq!(it.next(), Some(1));
-    assert_eq!(it.next(), Some(0));
-    assert_eq!(it.next(), None);
-    assert_eq!(it.next(), None);
-}
-
-#[test]
-fn make_iter_test() {
-    let mut i = 4u;
-    let mut it = make_iter(|| {
-        if i <= 0 {None}
-        else {i -= 1; Some(i)}
-    });
-    
-    assert_eq!(it.next(), Some(3));
-    assert_eq!(it.next(), Some(2));
-    assert_eq!(it.next(), Some(1));
-    assert_eq!(it.next(), Some(0));
-    assert_eq!(it.next(), None);
-    assert_eq!(it.next(), None);
+    let _ = FuncIter::new(|| val);
 }
